15c64c9247871b3e1987ff3fa66fd2be3a7938d0
ZABILA PETRO <petrozabila@gmail.com>
2022-05-19 11:44:41 +0300

    fixes for HW 6

--- js/task-06.js
+++ js/task-06.js
@@ -6,13 +6,17 @@ refs.inputItem.addEventListener('blur', changeStyle);
 
 
 function changeStyle(event) {
-    if(refs.inputItem.classList.contains('invalid')) {
-        refs.inputItem.classList.remove('invalid');
-    }
+   
 
     if ((Number(refs.inputItem.dataset.length)) === event.currentTarget.value.length) {
+        if(refs.inputItem.classList.contains('invalid')) {
+            refs.inputItem.classList.remove('invalid');
+        }
         refs.inputItem.classList.add('valid');
     } else {
+        if(refs.inputItem.classList.contains('valid')) {
+            refs.inputItem.classList.remove('valid');
+        }
         refs.inputItem.classList.add('invalid');
     }
 };
